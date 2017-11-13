ab9d6855892573854390328b4288cf6cfa623ab6
pvazteixeira <pvt@mit.edu>
2017-11-13 14:52:25 -0500

    self-adjusting timing

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Lubell
 Arduino Code for Custom Lubell Pingers
+
+## Dependencies
+
+* [Modified WaveHC library](https://github.com/pvazteixeira/WaveHC) - clone this library into your sketchbook's libraries directory (e.g. `git clone https://github.com/pvazteixeira/WaveHC.git $HOME/sketchbook/libraries/WaveHC`).
+
+
+## Examples
+
+* `pps_chirp` - plays a hard-coded WAV file a configurable amount of time after a PPS trigger. The code should adjust for the (file-dependent) time required to seek and load the waveform.
--- pps_chirp/pps_chirp.ino
+++ pps_chirp/pps_chirp.ino
@@ -6,7 +6,7 @@
 #include <WaveUtil.h>
 
 // put pi in flash memory
-//const char pi[] PROGMEM = "3.1415926535897932384626433832795028841971693993751058209749";
+// const char pi[] PROGMEM = "3.1415926535897932384626433832795028841971693993751058209749";
 SdReader card;    // This object holds the information for the card
 FatVolume vol;    // This holds the information for the partition on the card
 FatReader root;   // This holds the information for the volumes root directory
@@ -17,10 +17,11 @@ WaveHC wave;      // This is the only wave (audio) object, since we will only pl
 */
 #define error(msg) error_P(PSTR(msg))
 
-const int PPSPin = 7;  // the number of the PPS Pin
+const int PPSPin = 3;  // the number of the PPS Pin
 const int ledPin = 13; // board-mounted LED pin - DOESN'T WORK: PIN IS RESERVED BY WAVESHIELD!!! 
 int PPSState = 0;      // variable for reading the PPS status
 char filename[20];
+unsigned long time1,time2,et,wait;
 //////////////////////////////////// SETUP
 
 void setup() {
@@ -43,8 +44,9 @@ void setup() {
   
   PgmPrintln("Files found:");
   root.ls();
-  pinMode(PPSPin, INPUT);
-  strcpy_P(filename, PSTR("chirp1.wav"));
+  pinMode(PPSPin, INPUT_PULLUP);
+  strcpy_P(filename, PSTR("chirp7.wav"));
+  wait=50000;
 }
 
 /////////////////////////////////// LOOP
@@ -101,7 +103,10 @@ void sdErrorCheck(void) {
 */
 void playcomplete(char *name) {
   playfile(name);
-  while (wave.isplaying);
+  while (wave.isplaying){
+        Serial.println(".");
+    delay(100);
+  }
   // see if an error occurred while playing
   sdErrorCheck();
 }
@@ -112,6 +117,7 @@ void playfile(char *name){
   if (wave.isplaying) {// already playing something, so stop it!
     wave.stop(); // stop it
   }
+    time1 = micros();
   if (!file.open(root, name)){
     PgmPrint("Could not open file ");
     Serial.print(name);
@@ -121,6 +127,15 @@ void playfile(char *name){
     PgmPrintln("Not a valid WAV");
     return;
   }
+  wave.load(); // takes a file-specific amount of time
+  time2 = micros();
+  et = time2-time1;
+  Serial.println(et); // 
+  unsigned long diff = static_cast<unsigned long>(wait-et);
+  Serial.println(diff);
+  delay(diff/1000);
+  delayMicroseconds(diff%1000); 
+  Serial.println("!");
   // ok time to play!
   wave.play();
 }
